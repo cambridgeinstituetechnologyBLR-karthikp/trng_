145a67c98a9a87379048fef0be2693969abfea4a
cambridgeinstituetechnologyBLR-karthikp <karthik.24ece@cambridge.edu.in>
2026-05-26 15:23:42 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,21 +1,12 @@
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge
+from cocotb.triggers import Timer
 
 @cocotb.test()
 async def test_project(dut):
 
-    clock = Clock(dut.clk, 10, units="ns")
-    cocotb.start_soon(clock.start())
+    dut.ui_in.value = 5
+    dut.uio_in.value = 0
 
-    dut.rst_n.value = 0
+    await Timer(1, units="ns")
 
-    for _ in range(5):
-        await RisingEdge(dut.clk)
-
-    dut.rst_n.value = 1
-
-    for _ in range(20):
-        await RisingEdge(dut.clk)
-
-    dut._log.info(f"TRNG output: {dut.uo_out.value}")
+    assert dut.uo_out.value == 5
