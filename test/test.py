8985013b73fd36eca0fb9fdfb271fd5d40bcaaff
cambridgeinstituetechnologyBLR-karthikp <karthik.24ece@cambridge.edu.in>
2026-05-26 15:26:34 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,12 +1,27 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+
 
 @cocotb.test()
-async def test_project(dut):
+async def test_trng(dut):
 
-    dut.ui_in.value = 5
+    dut._log.info("Starting TRNG Test")
+
+    clock = Clock(dut.clk, 10, units="us")
+    cocotb.start_soon(clock.start())
+
+    dut.ena.value = 1
+    dut.ui_in.value = 0
     dut.uio_in.value = 0
 
-    await Timer(1, units="ns")
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 5)
 
-    assert dut.uo_out.value == 5
+    dut.rst_n.value = 1
+
+    await ClockCycles(dut.clk, 50)
+
+    dut._log.info(f"Random Output: {dut.uo_out.value}")
+
+    assert dut.uo_out.value is not None
